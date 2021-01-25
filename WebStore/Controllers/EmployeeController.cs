d595fa646035c754cb64ef443cf94198a91eb16b
Максим-1987 <max-tenigin1987@mail.ru>
2021-01-25 17:17:59 +0300

    Создание своего контроллера для Employees , перенос основных действий из контроллера Home + добавление в меню Shop ссылки для Employees и PhoneList

---
+++ WebStore/Controllers/EmployeeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly List<EmployeeView> _employees = new List<EmployeeView>
+        {
+             new EmployeeView
+            {
+                Id = 1,
+                FirstName = "Василий" ,
+                SurName = "Путин" ,
+                Patronymic = "Владимирович" ,
+                Age = 73,
+                Position = "Президент"
+            },
+            new EmployeeView
+            {
+                Id = 2,
+                FirstName = "Дмитрий" ,
+                SurName = "Песков" ,
+                Patronymic = "Федорович" ,
+                Age = 30,
+                Position = "Заместитель президента"
+            }
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Employees()
+        {
+            return View(_employees);
+        }
+        public IActionResult EmployeeDetails(int id)
+        {
+            //Получаем сотродника по Id
+            var employee = _employees.FirstOrDefault(t => t.Id == id);
+            //Проверка на существует ли данный сотрудник
+            if (employee == null)
+                return NotFound();
+            //Возвращаем сотрудника
+            return View(employee);
+        }
+    }
+}
--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -9,44 +9,10 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly List<EmployeeView> _employees = new List<EmployeeView>
-        {
-            new EmployeeView
-            {
-                Id = 1,
-                FirstName = "Василий" ,
-                SurName = "Путин" ,
-                Patronymic = "Владимирович" ,
-                Age = 73,
-                Position = "Президент"
-            },
-            new EmployeeView
-            {
-                Id = 2,
-                FirstName = "Дмитрий" ,
-                SurName = "Песков" ,
-                Patronymic = "Федорович" ,
-                Age = 30,
-                Position = "Заместитель президента"
-            }
-        };
+
         public IActionResult Index()
         {
             return View();
-        }
-        public IActionResult Employees()
-        {
-            return View(_employees);
-        }
-        public IActionResult EmployeeDetails(int id)
-        {
-            //Получаем сотродника по Id
-            var employee = _employees.FirstOrDefault(t => t.Id == id);
-            //Проверка на существует ли данный сотрудник
-            if (employee == null)
-                return NotFound();//404 NotFound
-            //Возвращаем сотрудника
-            return View(employee);
         }
 
 
