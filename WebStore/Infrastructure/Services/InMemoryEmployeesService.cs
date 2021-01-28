72053c9bb1e97d4d4f1b252bc0817a3c570caee2
Максим-1987 <max-tenigin1987@mail.ru>
2021-01-28 18:43:13 +0300

    подготовка основной инфраструктуры для добавления , редактирования и изменения

---
+++ WebStore/Infrastructure/Services/InMemoryEmployeesService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Services
+{
+    public class InMemoryEmployeesService : IEmployeesService
+    {
+        private readonly List<EmployeeView> _employees = new List<EmployeeView>
+        {
+                   new EmployeeView
+            {
+                Id = 1,
+                FirstName = "Василий" ,
+                SurName = "Путин" ,
+                Patronymic = "Владимирович" ,
+                Age = 73,
+                Position = "Президент"
+            },
+            new EmployeeView
+            {
+                Id = 2,
+                FirstName = "Дмитрий" ,
+                SurName = "Песков" ,
+                Patronymic = "Федорович" ,
+                Age = 30,
+                Position = "Заместитель президента"
+            }
+        };
+
+        public void AddNew(EmployeeView employeeView)
+        {
+            employeeView.Id = _employees.Max(e => e.Id) + 1;
+            _employees.Add(employeeView);
+        }
+
+        public void Commit()
+        {
+            //TODO
+        }
+
+        public void Delete(int id)
+        {
+            var employee = GetById(id);
+            if(employee is null)
+                return;
+            _employees.Remove(employee);
+        }
+
+        public IEnumerable<EmployeeView> GetAll()
+        {
+            return _employees;
+        }
+
+        public EmployeeView GetById(int id)
+        {
+            return _employees.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
